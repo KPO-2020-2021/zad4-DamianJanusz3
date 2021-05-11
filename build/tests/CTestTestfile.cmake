a45ce31dbd59751e53180e354999846822bcf42f
Damian Janusz <259253@student.pwr.edu.pl>
2021-05-11 19:35:44 +0200

    t0 i t1poprawione

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lapidary/zad4-DamianJanusz3/tests
-# Build directory: /home/lapidary/zad4-DamianJanusz3/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(program.unit_tests "/home/lapidary/zad4-DamianJanusz3/build/unit_tests")
-set_tests_properties(program.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/lapidary/zad4-DamianJanusz3/tests/CMakeLists.txt;29;add_test;/home/lapidary/zad4-DamianJanusz3/tests/CMakeLists.txt;0;")
